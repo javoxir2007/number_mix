067a33dd765e2ae216bdbf7f3768b45d9db7b57f
DilbekErgashev1 <ergashevdilbek9@gmail.com>
2022-07-30 01:05:01 -0400

    kichik son

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-    int a = 1;
-    int b = 5;
-      if (a > b) {
-              System.out.println (b);
-      } else if ( b > a) {
-              System.out.println (a);
-      }else {
-              System.out.println ("t");
-      }
+        int a = 1;
+        int b = 5;
+        if (a > b) {
+            System.out.println(b);
+        } else {
+            System.out.println(a);
+        }
 
     }
 }
